2b1f1ac6a23297b0856b2e09d3b8b7c77f84717e
Rosa Pinccay <rmpincay@espol.edu.ec>
2020-01-24 01:22:15 -0500

    update db

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:3oPWq+nb6PRRLxGDt7P4wxchtPtp+QHkM/mwBVkF2AU='),
 
     'cipher' => 'AES-256-CBC',
 
